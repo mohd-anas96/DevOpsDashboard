5e74bfa425944a9fc00c3e5ae1801a1a3de66a08
Anas <mohd.anas4@globallogic.com>
2026-07-31 19:52:53 +0530

    New version

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
     <header className="bg-slate-900 text-white px-8 py-5 flex justify-between items-center shadow-md">
       <div>
         <h1 className="text-3xl font-bold">
-          DevOps Dashboard v2
+          DevOps Dashboard v3
         </h1>
 
         <p className="text-sm text-gray-300">
